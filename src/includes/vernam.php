8cf51f62ee03f1b0f09f56b2134d7f8fb0970858
laidet_r <rodolphe.laidet@epitech.eu>
2017-08-05 17:39:46 +0200

    Add encode/decode functions for Vernam in it's own file

---
+++ src/includes/vernam.php
@@ -0,0 +1,19 @@
+<?php
+
+function encode_vernam($input = "", $key = "")
+{
+    $output_array = [];
+
+    $output = implode($output_array);
+    $output = "VERNAM ENCODED";
+    return $output;
+}
+
+function decode_vernam($input = "", $key = "")
+{
+    $output_array = [];
+
+    $output = implode($output_array);
+    $output = "VERNAM DECODED";
+    return $output;
+}
